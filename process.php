86d612d0853b6efe8ccb3948fc77e34aa8f04486
jidfindia <77541291+jidfindia@users.noreply.github.com>
2021-06-15 13:46:15 +0530

    Update process.php

--- process.php
+++ process.php
@@ -3,8 +3,7 @@ header("Pragma: no-cache");
 header("Cache-Control: no-cache");
 header("Expires: 0");
 //Fetch Value from Form Submitted
-// Parameters Accouring to Form Submitted at index.php -- Start
-$custname=$_POST['CUST_NAME'];  
+$custname=$_POST['CUST_NAME'];
 $custemail=$_POST['CUST_EMAIL'];
 $custmobile=$_POST['CUST_MOBILE'];
 $custaddressline1=$_POST['CUST_ADDRESS_LINE1'];
@@ -15,14 +14,13 @@ $custaddresscountry=$_POST['CUST_ADDRESS_COUNTRY'];
 $custaddresspostalcode=$_POST['CUST_ADDRESS_POSTAL_CODE'];
 $orderid=$_POST['ORDER_ID'];
 $ordervalue=$_POST['TXN_AMOUNT'];
-// Parameters Accouring to Form Submitted at index.php -- End
 //Fetch Value From KP Environment file
-require_once 'config.php';
+require_once 'kpenv/config.php';
 $paramList = array();
-$paramList["KP_ENVIRONMENT"] = KP_ENVIRONMENT; /// Set in kpenv/config.php file
-$paramList["KPMID"] = KPMID; /// Set in kpenv/config.php file
-$paramList["KPMIDKEY"] = KPMIDKEY; /// Set in kpenv/config.php file
-$paramList["TXN_CURRENCY"] = TXN_CURRENCY;/// Set in kpenv/config.php file 
+$paramList["KP_ENVIRONMENT"] = KP_ENVIRONMENT;
+$paramList["KPMID"] = KPMID;
+$paramList["KPMIDKEY"] = KPMIDKEY;
+$paramList["TXN_CURRENCY"] = TXN_CURRENCY;
 ///Create Customer From API Pass Customer Parameters to https://pispp.kwikpaisa.com/API/v1/CreateCustomer
 $curl = curl_init();
 curl_setopt_array($curl, array(
@@ -41,7 +39,9 @@ curl_close($curl);
 $response;
 //Make Variable of Customer ID Received From API Call
 $customerId=json_decode(($response),true);
-$customerIdvalue=$customerId["CUST_KP_ID"];
+$CustomerAPIStatus=$customerId["status"];
+if ($CustomerAPIStatus == 'success') {$customerIdvalue=$customerId["CUST_KP_ID"];} else {echo $response; exit();}
+
 
 ///Now Create Order ID and Payment Token By Passing Parameters To https://pispp.kwikpaisa.com/API/v1/Order
 $curl = curl_init();
@@ -56,18 +56,20 @@ curl_setopt_array($curl, array(
   CURLOPT_CUSTOMREQUEST => 'POST',
   CURLOPT_POSTFIELDS => array('KP_ENVIRONMENT' => $paramList["KP_ENVIRONMENT"],'KPMID' => $paramList["KPMID"],'KPMIDKEY' => $paramList["KPMIDKEY"],'CUST_KP_ID' => $customerIdvalue,'TXN_CURRENCY' => $paramList["TXN_CURRENCY"],'TXN_AMOUNT' => $ordervalue,'ORDER_ID' => $orderid),
 ));
-$response = curl_exec($curl);
+$responseOrder = curl_exec($curl);
 curl_close($curl);
-$response;
 //Make Variable of Customer ID Received From API Call
-$OrderDetails=json_decode(($response),true);
-$KP_Txn_OrderID=$OrderDetails["KP_Txn_OrderID"];
+$OrderDetails=json_decode(($responseOrder),true);
+$OrderAPIStatus=$OrderDetails["status"]; 
+if ($OrderAPIStatus == 'success') 
+{$KP_Txn_OrderID=$OrderDetails["KP_Txn_OrderID"];
 $KP_Txn_Signature=$OrderDetails["KP_Txn_Signature"];
-$KP_Txn_Token=$OrderDetails["KP_Txn_Token"];
+$KP_Txn_Token=$OrderDetails["KP_Txn_Token"];} else {echo $responseOrder; exit();}
 ?>
 <title>Just Passing Main paramList to KP Gateway Log</title><body>
 <center><h1>Please do not refresh and close this page/window...</h1></center>
-<form id="paymentform" method="post" action="<?php echo KP_TXN_URL ?>" name="f1">
+<form id="paymentform" method="post" action="<?php echo KP_TXN_URL ?>" 
+name="<?php if ($OrderAPIStatus || $CustomerAPIStatus == 'success') {echo 'f1';} else {}?>">
     <input type="hidden" name="KPMID" value="<?php echo KPMID ?>"/>
     <input type="hidden" name="CUST_KP_ID" value="<?php echo $customerIdvalue ?>"/>
     <input type="hidden" name="KP_Txn_OrderID" value="<?php echo $KP_Txn_OrderID ?>"/>
@@ -76,3 +78,4 @@ $KP_Txn_Token=$OrderDetails["KP_Txn_Token"];
     <input type="hidden" name="KP_Return_URL" value="<?php echo CALLBACK_URL ?>"/>
     <script type="text/javascript">document.f1.submit();</script>
   </form>
+ 
